0964611f375a041be734dd44bd50b6dfa98e77c8
Prince Valerie <148141403+princevalerie@users.noreply.github.com>
2025-03-20 02:37:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash",
+    model_name="gemini-2.0-flash-lite",
     generation_config=generation_config
 )
 
